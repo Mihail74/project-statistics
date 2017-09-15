131e14c672c1fb51b6841de4895ec06f61138c2f
Kardaev Mikhail <Kardaev.74@gmail.com>
2017-09-15 19:08:51 +0500

    add atom-beautify

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,9 @@ import Hello from '@/components/Hello.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Hello',
+    component: Hello
+  }]
 })
