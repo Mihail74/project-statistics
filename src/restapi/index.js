a87f89ce29e3f8d8e04f531dfddb651f5087e700
Mikhail <kardaev.74@gmail.com>
2017-11-25 18:00:49 +0500

    sime fix

--- src/restapi/index.js
+++ src/restapi/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "@/store";
 import router from "@/router";
 import authService from "@/services/authorization";
-import ApiError from "@/restApi/ApiError.js";
+import ApiError from "@/restapi/ApiError.js";
 
 
 class RestApi {
