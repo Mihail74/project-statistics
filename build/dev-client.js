a9ad8b97e8c1e3131cc0e6a6652ebd310be696bd
Mikhail <kardaev.74@gmail.com>
2018-01-22 19:32:16 +0500

    add paging

--- build/dev-client.js
+++ build/dev-client.js
@@ -5,6 +5,7 @@ var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
 window.__APP_CONFIG__ = __APP_CONFIG__
 
 hotClient.subscribe(function (event) {
+  console.log(event);
   if (event.action === 'reload') {
     window.location.reload()
   }
---
+++ src/pages/matches/list/MatchesFilter.vue
@@ -0,0 +1,47 @@
+<template>
+  <md-whiteframe class="whiteframe">
+    <h2 class="md-body-2">Фильтры</h2>
+    <div>
+      <md-checkbox id="onlyMyMatches" name="onlyMyMatches" v-model="onlyMyMatches">Только мои команды</md-checkbox>
+      <team-select label="Обязательная команда участник" @change="changeTeam"/>
+      <md-button class="md-raised md-primary" @click="filter">Применить</md-button>
+    </div>
+
+  </md-whiteframe>
+</template>
+
+<script>
+import TeamSelect from "@/components/teams/select";
+
+export default {
+    components: {
+        TeamSelect
+    },
+
+    data() {
+        return {
+            onlyMyMatches: false,
+            teamID: null
+        }
+    },
+
+    methods: {
+      filter(){
+        this.$emit("filter", {
+          onlyMyMatches: this.onlyMyMatches,
+          teamID: this.teamID
+        });
+      },
+      changeTeam(selectedTeam) {
+        this.teamID = selectedTeam.id
+      }
+    }
+}
+</script>
+
+<style>
+.whiteframe {
+  padding-top: 8px;
+  padding-left: 24px;
+}
+</style>
